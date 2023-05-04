b0fb79056483d347d95fdf2a197b1196d87975f4
Alecsander <alecsanderdias@hotmail.com>
2023-05-04 19:16:35 -0300

    "Iniciando o curso C#: usando herança e implementando interfaces"

---
+++ curso-OO-CSharp/bytebank_ADM/Program.cs
@@ -0,0 +1,10 @@
+﻿using bytebank_ADM.Funcionarios;
+
+Funcionario alec = new Funcionario();
+alec.Nome = "Alex Dias";
+alec.Salario = 1500;
+alec.CPF = "123";
+Console.WriteLine(alec.Nome);
+Console.WriteLine(alec.CPF);
+Console.WriteLine(alec.Salario);
+Console.WriteLine(alec.GetBonificacao());
